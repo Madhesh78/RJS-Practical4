8ae22e239c2b12e41f78e6774a2898ee212ab42a
MADHESH <kulla7364@gmail.com>
2026-07-08 09:20:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Complete the function using object destructuring.
 
 function getStudentDetails(students) {
-    // Write your code here
+        return students.map(({ name, age, grade }) => {
+        return `${name} is ${age} years old and has grade ${grade}`;
+ });
 }
-
-module.exports = getStudentDetails;
+const students = [
+  { name: "Alice", age: 20, grade: "A" },
+  { name: "Bob", age: 22, grade: "B" }];
+console.log(getStudentDetails(students));
